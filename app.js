5ddda4aa787b927309cc32bf00d29986008fc0b4
Kancho Paudel <paudel.sinjan@gmail.com>
2021-01-30 23:09:36 +0545

    Create and add Modules to project

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
+
+//User modules
+const User = require('./Modules/Users');
 
 //Middleware
 app.use(express.json());
@@ -16,8 +20,25 @@ app.get("/",(req,res)=>{
     res.send("Welcome to the app\n");
 })
 
-app.get('/register',(req,res)=>{
+app.post('/register',(req,res)=>{
+    console.log(req.body);
 
+    let newUser = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password
+    });
+     bcrypt.genSalt(10,(err,salt)=>{
+        bcrypt.hash(newUser.password,salt,(err,hash)=>{
+            newUser.password = hash;
+            newUser.save().then(savedUser => {
+                res.send("new user saved");
+            }).catch(err => {
+                console.log("Err saving user: "+ err);
+            });
+        });
+     });
 })
 
 app.listen(9090,()=>{
